4bd2b571b328dd526d50adbc9a3b639d6d08a4e8
dhruv289 <38131147+dhruv289@users.noreply.github.com>
2018-10-26 16:48:34 +0800

    Update models.py

--- COMP3297_ShakeShack/AS-P/content/models.py
+++ COMP3297_ShakeShack/AS-P/content/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class user(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    clinic = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email_ID = models.CharField(max_length=200)
+    token code = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
