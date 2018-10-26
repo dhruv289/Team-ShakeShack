bd88e7619bccb60bb1ce4de36c906b87059428e6
AlanBinn <44283053+AlanBinn@users.noreply.github.com>
2018-10-26 22:20:51 +0800

    Distance
    Two functions added to store the information of the location and calculating the distance given the two places.

--- COMP3297_ShakeShack/AS-P/content/models.py
+++ COMP3297_ShakeShack/AS-P/content/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import mpu #Install via "pip install mpu --user"
 
 # Create your models here.
 
@@ -13,3 +14,24 @@ class user(models.Model):
 
     def __str__(self):
         return self.username
+
+class Location(models.Model):
+	name = models.CharField(max_length=200)
+	latitude = models.FloatField
+	longitude = models.FloatField
+	altitude = models.IntegerField
+	
+	def _str_(self);
+		return self.name
+
+class Distance(models.Model):
+	placeA = models.ForeignKey(Location, on_delete=models.CASCADE)
+	placeB = models.ForeignKey(Location, on_delete=models.CASCADE)
+	distance = models.FloatField(default = 0.0)
+	
+	def save(self, *args, **kwargs):
+		self.distance = self.calculate()
+		super(Distance, self).save(*args, **kwargs)
+	
+	def calculate(self):
+		return mpu.haversine_distance((placeA.latitude, placeA.longitude), (placeB.latitude, placeB.longitude))
