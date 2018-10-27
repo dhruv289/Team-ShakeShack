d5eb08e726c48888d76b13124a247165563fff50
gohkevin <44095759+gohkevin@users.noreply.github.com>
2018-10-27 14:27:13 +0800

    Update models.py

--- COMP3297_ShakeShack/AS-P/content/models.py
+++ COMP3297_ShakeShack/AS-P/content/models.py
@@ -13,3 +13,20 @@ class user(models.Model):
 
     def __str__(self):
         return self.username
+    
+class cart(models.Model):
+    username = models.ForeignKey(user, on_delete=modes.CASCADE)
+    item = models.DecimalField(max_digits=5)
+    quantity = models.DecimalField(max_digits=5)
+    
+    def __str__(self):
+        return f'{self.username} ({self.item})'
+
+class content(models.Model):
+    username = models.ForeignKey(user, on_delete=modes.CASCADE)
+    item = models.DecimalField(max_digits=5)
+    quantity = models.DecimalField(max_digits=5)
+    orderID = models.ForeignKey(orders, on_delete=modes.CASCADE)
+    
+    def __str__(self):
+        return f'{self.username} ({self.item})'
