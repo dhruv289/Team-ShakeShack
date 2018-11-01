2cb205af0e78a47734d962ded11a23dc979803f7
dhruv289 <38131147+dhruv289@users.noreply.github.com>
2018-11-02 00:46:14 +0800

    Update views.py

--- AS-P/AS-P/Pages/views.py
+++ AS-P/AS-P/Pages/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from Orders.models import Order
+from Inventory.models import Item
 # Create your views here.
 
 def add_cart_view(request, *args, **kwargs):
-	return render(request,"add_cart_view.html",{});
+	obj = Item.objects.get(item_id=1)
+	context = {
+		'object' : obj
+	}
+	return render(request,"add_cart_view.html", context);
 
 def client_home_view(request, *args, **kwargs):
 	return render(request,"client_home_view.html",{});
@@ -14,7 +19,6 @@ def dispatcher_home_view(request, *args, **kwargs):
 		'object' : obj
 	} 
 	return render(request,"dispatcher_home_view.html",context);
-
 
 def order_success_view(request, *args, **kwargs):
 	return render(request,"order_success_view.html",{});
