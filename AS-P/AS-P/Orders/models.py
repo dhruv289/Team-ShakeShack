c734635316e4043982a023a230ea3814f7f43fb5
dhruv289 <38131147+dhruv289@users.noreply.github.com>
2018-11-03 17:15:24 +0800

    Update models.py

--- AS-P/AS-P/Orders/models.py
+++ AS-P/AS-P/Orders/models.py
@@ -5,20 +5,21 @@ from Inventory import models as Inventory_models
 
 # Create your models here.
 class Order(models.Model):
-	orderID=models.IntegerField()
+	order_id=models.DecimalField(max_digits=4,decimal_places=2)
 	owner=models.ForeignKey(users_models.user,on_delete=models.CASCADE)
-	#username=user.objects.select_related().get(owner.username)
 	status=models.CharField(max_length=200)
 	priority=models.CharField(max_length=200)
-	#location=user.objects.select_related().get(owner.location)
+	creation_time = models.DateTimeField(auto_now_add=True)
+	dispatch_time = models.DateTimeField(null = True)
+
 	def __str__(self):
-		return self.orderID
+		return str(self.order_id)
 
 class content(models.Model):
     username = models.ForeignKey(users_models.user, on_delete=models.CASCADE)
     item_id = models.ForeignKey(Inventory_models.Item, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    quantity = models.DecimalField(max_digits=4,decimal_places=0)
     orderID = models.ForeignKey(Order, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f'{self.username} ({self.item})'
+        return f'{self.username} ({self.item_id})'
