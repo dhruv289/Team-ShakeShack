b354f70c5fef4e6da1af57ec064f46804d550e1b
gohkevin <44095759+gohkevin@users.noreply.github.com>
2018-11-19 19:55:37 +0800

    Update models.py

--- AS-P/users/models.py
+++ AS-P/users/models.py
@@ -8,8 +8,15 @@ class user(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
     email_ID = models.CharField(max_length=200) #replace with email field?
-    token_code = models.CharField(max_length=200)
+    token_code = models.ForeignKey(users.tokencode, on_delete=models.CASCADE)
     location=models.ForeignKey(Location_Data_models.Location, on_delete=models.CASCADE)
-
+    
     def __str__(self):
         return self.username
+    
+class tokencode(models.Model):
+    token_code = models.CharField(max_length=200)
+    email_ID = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.token_code
